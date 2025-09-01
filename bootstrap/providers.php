7199ac4a3e47d0380833064715a428ffa1451946
Stanko172 <stanko.bebek00@gmail.com>
2025-09-01 20:43:42 +0200

    feat: enable pgsql multi schema tenancy

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,5 +4,5 @@ declare(strict_types=1);
 
 return [
     App\Providers\AppServiceProvider::class,
-    \Stancl\Tenancy\TenancyServiceProvider::class,
+    App\Providers\TenancyServiceProvider::class,
 ];
